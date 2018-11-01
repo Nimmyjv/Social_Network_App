d4af934b15f283f74e401327eeea6b7dcacfb8ad
Vishnu V V <nimmy@redpanthers.co>
2018-11-02 01:50:08 +0530

    Votable feature

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,10 @@
 class Photo < ApplicationRecord
-  mount_uploader :avatar, AvatarUploader
   belongs_to :user
+  acts_as_votable
+
+  default_scope -> { order('created_at DESC') }
+
+  mount_uploader :avatar, AvatarUploader
+
+  validates_presence_of :user
 end
